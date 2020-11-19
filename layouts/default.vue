56fb4693afdea64d9887468a854918ce14cb2fbd
unknown <chris@nextgm.ca>
2020-11-19 01:04:46 +0100

    conception change... delete routes, styling search button in navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,10 @@
   <div class="website_container">
     <header>
       <nav class="website_navigation">
-        <input class="navigation_item" type="text" value="" placeholder="Search..">
-        <article class="navigation_item">
-          <NuxtLink to="/">Blog</NuxtLink>
-        </article>
-        <article class="navigation_item">
-          <NuxtLink to="/about">About</NuxtLink>
-        </article>
+        <div class="search_box">
+          <input type="text" value="" placeholder="Search for anything.." />
+          <button type="button">&#10095;</button>
+        </div>
       </nav>
     </header>
     <Nuxt />
@@ -61,48 +58,75 @@ body {
   max-width: 100vw;
   min-height: 100vh;
   height: auto;
-  overflow: hidden;
+  overflow-x: hidden;
 
   & .website_container {
     min-width: 330px;
     width: 100%;
     max-width: 820px;
-    height: 100vh;
+    height: 120vh;
     margin: 0 auto;
 
     & header {
+      width: 100%;
+      height: 50px;
+      position: relative;
+
       & .website_navigation {
         width: 100%;
-        height: 70px;
+        height: 50px;
+        max-width: 820px;
         display: flex;
         justify-content: flex-end;
         align-items: center;
+        position: fixed;
+        top: 0;
 
-        & input {
-          border-radius: 5px;
-          outline: none;
-          border: 3px solid #dedede;
-          background-color: #f5f5f5;
-          width: 120px !important;
-        }
+        & .search_box {
+          display: flex;
+          flex-direction: wrap;
+          flex-wrap: nowrap;
+          justify-content: flex-end;
+          align-items: center;
+          min-width: 185px;
+          width: 70%;
+          max-width: 500px;
+          margin-right: 20px;
 
-        & .navigation_item {
-          width: 75px;
-          height: 35px;
-          margin-right: 15px;
+          & input {
+            border-radius: 5px 0 0 5px;
+            outline: none;
+            border: 2px solid #dedede;
+            background-color: #fff;
+            min-width: 150px;
+            width: calc(100% - 30px);
+            max-width: 475px;
+            height: 39px;
+            font-size: 16.5px;
+            color: #dedede;
 
-          & a {
+            &::placeholder {
+              padding-left: 10px;
+              color: #dedede;
+            }
+          }
+
+          & button {
+            height: 39px;
+            width: 30px;
+            border-top: 2px solid #dedede;
+            border-right: 2px solid #dedede;
+            border-bottom: 2px solid #dedede;
+            border-left: none;
+            outline: none;
+            background-color: #dedede;
+            font-size: 30px;
+            font-weight: bold;
+            color: #fff;
+            cursor: pointer;
             display: flex;
             justify-content: center;
             align-items: center;
-            width: 100%;
-            height: 100%;
-            background-color: cornsilk;
-            border-radius: 5px;
-
-            &.nuxt-link-active {
-              background-color: orange;
-            }
           }
         }
       }
