c35e94a1f2fea90b95a620403e83776d82ec6cc5
unknown <chris@nextgm.ca>
2020-11-20 19:36:33 +0100

    styling components

---
+++ components/ArticlePreview.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="article_container">
+    <p class="article_title">something</p>
+    <p class="brief_description">something</p>
+  </section>
+</template>
+
+<style lang="scss">
+.article_container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  & p {
+    padding-left: 10px;
+    padding-right:10px;
+
+    &.article_title {
+      font-size: 17.5px;
+      margin-bottom: 5px;
+      padding-bottom:2px;
+      padding-top: 8px;
+      border-bottom: 2px solid black;
+    }
+
+    &.brief_description{
+      font-size: 16.5px;
+      padding-top:5px;
+      padding-bottom:10px;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
-    <nav class="page_title_wraper"><p class="hello_message">Go, get anything You dreamed of</p></nav>
-    <main><ArticleThumbnail /><ArticleThumbnail /><ArticleThumbnail /><ArticleThumbnail /><ArticleThumbnail />
+    <nav class="page_title_wraper">
+      <p class="hello_message">Go, get anything You dreamed of</p>
+    </nav>
+    <main>
+      <ArticlePreview /><ArticlePreview /><ArticlePreview /><ArticlePreview /><ArticlePreview />
     </main>
     <footer></footer>
   </div>
@@ -23,39 +26,35 @@ export default Vue.extend({})
     width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     height: 40px;
     margin: 30px 0;
-    text-align: left;
-    background-color: #c0c0c0;
-    border-radius: 5px 5px 0  0;
+
     & .hello_message {
       font-size: 16.5px;
-      color: #fff;
+      color: black;
       font-weight: 550;
+      padding: 2px 10px;
+      border-bottom: 2px solid black;
     }
   }
   & main {
     width: 100%;
     min-height: 40vh;
-    height:auto;
-    background-color:#c0c0c0;
+    height: auto;
+    background-color: #c0c0c0;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: space-between;
-      & section:nth-child(1){
-    flex-basis: 100%;
-    background-color:darkmagenta;
-    margin-bottom: 30px;
-  }
-  & section:nth-child(n + 2){
-    min-width:300px;
-    flex-basis: 40%;
-    background-color:darkred;
-    margin-bottom: 15px;
-  }
+
+    & section {
+      min-width: 300px;
+      width: 40%;
+      background-color: darkred;
+      margin-bottom: 15px;
+    }
   }
 }
 // .title {
